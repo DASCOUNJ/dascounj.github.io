927da73b20a76cbd0d1c4ffe764ce57121243de7
Hyuto <wahyusetianto19@gmail.com>
2021-02-15 04:42:54 +0700

    Updates

---
+++ beta/js/about.bc8197c6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],o={name:"About",components:{},created:function(){document.title="About"}},c=o,s=n("2877"),i=Object(s["a"])(c,a,u,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=about.bc8197c6.js.map
