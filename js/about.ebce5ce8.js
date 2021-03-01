a2bfc67cc18a24604a8e9582f43d627b8954d014
Hyuto <wahyusetianto19@gmail.com>
2021-03-01 20:48:25 +0700

    Updates

--- js/about.ebce5ce8.js
+++ js/about.ebce5ce8.js
@@ -1,2 +1,52 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"307c":function(t,e,n){t.exports=n.p+"img/on-development.82fbafcf.gif"},"5f83":function(t,e,n){},f26d:function(t,e,n){"use strict";n("5f83")},f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("div",{staticClass:"wrapper-img"},[s("h2",{staticClass:"text"},[t._v("Under development")]),s("img",{staticClass:"image",attrs:{src:n("307c"),alt:"On Development"}})])])}],a={name:"About",components:{},created:function(){document.title="About"}},i=a,o=(n("f26d"),n("2877")),u=Object(o["a"])(i,s,c,!1,null,null,null);e["default"]=u.exports}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["about"], {
+        "307c": function (t, e, n) {
+            t.exports = n.p + "img/on-development.82fbafcf.gif"
+        },
+        "5f83": function (t, e, n) {},
+        f26d: function (t, e, n) {
+            "use strict";
+            n("5f83")
+        },
+        f820: function (t, e, n) {
+            "use strict";
+            n.r(e);
+            var s = function () {
+                    var t = this,
+                        e = t.$createElement;
+                    t._self._c;
+                    return t._m(0)
+                },
+                c = [function () {
+                    var t = this,
+                        e = t.$createElement,
+                        s = t._self._c || e;
+                    return s("div", {
+                        staticClass: "about"
+                    }, [s("div", {
+                        staticClass: "wrapper-img"
+                    }, [s("h2", {
+                        staticClass: "text"
+                    }, [t._v("Under development")]), s("img", {
+                        staticClass: "image",
+                        attrs: {
+                            src: n("307c"),
+                            alt: "On Development"
+                        }
+                    })])])
+                }],
+                a = {
+                    name: "About",
+                    components: {},
+                    created: function () {
+                        document.title = "About"
+                    }
+                },
+                i = a,
+                o = (n("f26d"), n("2877")),
+                u = Object(o["a"])(i, s, c, !1, null, null, null);
+            e["default"] = u.exports
+        }
+    }
+]);
 //# sourceMappingURL=about.ebce5ce8.js.map
