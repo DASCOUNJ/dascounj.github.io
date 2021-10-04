09e98d507b9bbbb8404264cc1392940164c3c7f8
Hyuto <wahyusetianto19@gmail.com>
2021-10-05 02:41:14 +0700

    Updates

---
+++ component---src-pages-404-js-4721db13164d935c9e0a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,a){a.r(t);var l=a(7294),n=a(7198),r=a(3751);t.default=function(e){var t=e.location;return l.createElement(n.Z,{location:t},l.createElement(r.Z,{title:"404: Not Found"}),l.createElement("div",{style:{textAlign:"center"}},l.createElement("h1",null,l.createElement("strong",null,"404")),l.createElement("p",null,"Maaf Halaman yang anda cari tidak tersedia.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4721db13164d935c9e0a.js.map
